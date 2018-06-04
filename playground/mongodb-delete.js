6ebddbe92f037f09908ec38d14d63f0d1a985412
Nikola Zarkovic <nikolica.nidzica@gmail.com>
2018-06-04 13:17:12 +0200

    added deletition functionality

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,23 @@
+const {MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp' , (err, client) => {
+  if(err){
+    return console.log('Error while connecting to local MongoDB server');
+  }
+  console.log('Connected to MongoDB server successfully!');
+  const db = client.db('TodoApp');
+
+  //deleteMany
+    // db.collection('Todos').deleteMany({text: 'Napravi zdrala'}).then((result)=>{
+    //   console.log(result);
+    // });
+  //deleteOne
+    // db.collection('Todos').deleteOne({text: 'Napravi zdrala'}).then((result)=>{
+    //   console.log(result);
+    // });
+  //findOneAndDelete
+  db.collection('Todos').findOneAndDelete({text: 'Napravi zdrala'}).then((result)=>{
+    console.log(result);
+  });
+  //client.close();
+});
